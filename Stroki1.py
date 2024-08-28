937887d2afca1a6c8542104a544a142ad77b302c
Alvago-maestro <alvago-maestro@yandex.ru>
2024-08-28 23:03:43 +0600

    Add files via upload

---
+++ Stroki1.py
@@ -0,0 +1,7 @@
+example = 'Программа'
+print(example[0])
+print(example[-1])
+kolishestvo_simvolov = (len(example) // 2)
+print(example[kolishestvo_simvolov:])
+print(example[::-1])
+print(example[::2])
